83940f37348335836e0a17558553cf6338d1791b
AbramBuck <82072155+AbramBuck@users.noreply.github.com>
2024-07-24 15:44:40 -0500

    Finished with 3 & 4

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,24 @@
 function changeContext(func, obj) {
-  // Your code here 
+  
+  //Function you want to call.bind(context, parameter)
+  const funcToCall = func.bind(obj);
+  return funcToCall();
 }
+
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
